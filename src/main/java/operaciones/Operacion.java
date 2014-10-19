b6f8307c5161762ee5f969dc8c74206bb2fbcd18
pablo-corrales <charliequiro>
2014-10-19 11:06:22 +0200

    Se refactoriza clase Operacion

--- src/main/java/operaciones/Operacion.java
+++ src/main/java/operaciones/Operacion.java
@@ -6,17 +6,21 @@ public abstract class Operacion extends Expresion {
     private char operador;
 
    
-    public Operacion(final Expresion operando1, final char operador, final Expresion operando2) {
+    public Operacion(Expresion operando1, char operador, Expresion operando2) {
         this.operando1=operando1;
         this.operador = operador;
         this.operando2=operando2;
         
     }
+    
+    
 
     @Override
     protected abstract int operar();
+    
+    
     @Override
-    protected final String toSring() {
+    protected String toSring() {
         return "(" + operando1.toSring() + operador + operando2.toSring() + ")";
     }
 

16d374d4c6ad979c07610977a18f9a2b6d8bd1a5
pablo-corrales <charliequiro>
2014-10-19 11:05:41 +0200

    Se refactoriza clase Addition

--- src/main/java/operaciones/Addition.java
+++ src/main/java/operaciones/Addition.java
@@ -3,11 +3,13 @@ package operaciones;
 public class Addition extends Operacion {
 
   
-    public Addition(final Expresion op1, final Expresion op2) {
+    public Addition(Expresion op1, Expresion op2) {
         super(op1, '+',op2);
     }
+    
+    
     @Override
-    protected final int operar() {
+    protected int operar() {
         return  operando1.operar() + operando2.operar();
     }
 

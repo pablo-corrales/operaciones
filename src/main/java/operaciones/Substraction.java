d5cfb8c2cf878184f1d91b4a17cf0fee08b90df7
pablo-corrales <charliequiro>
2014-10-19 11:06:45 +0200

    Se refactoriza clase Substraction

--- src/main/java/operaciones/Substraction.java
+++ src/main/java/operaciones/Substraction.java
@@ -3,12 +3,14 @@ package operaciones;
 public class Substraction extends Operacion {
 
   
-    public Substraction(final Expresion op1, final Expresion op2) {
+    public Substraction(Expresion op1, Expresion op2) {
         super(op1, '-',op2);
     }
+    
+    
     @Override
-    protected final int operar() {
-        return  operando1.operar() + operando2.operar();
+    protected int operar() {
+        return  operando1.operar() - operando2.operar();
     }
 
 }
